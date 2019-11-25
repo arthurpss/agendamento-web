b1d6b9a5f51908ab10b4f70dcfec93017b147855
guilherme.santana <santanaguilherme2000@gmail.com>
2019-11-25 14:14:38 -0300

    Alteracao da cor geral do background do sistema e alteracoes .css

--- src/components/especialidades/index.js
+++ src/components/especialidades/index.js
@@ -1,22 +1,23 @@
 import React from "react"
+import "./styles.css"
 
 const especialidades = [
     {
 
         titulo: "Pediatria",
-        descricao: "Cuida de criança",
+        descricao: "Especialidade da Medicina dedicada ao cuidado da saúde de crianças, adolescentes e pré-adolescentes. Sua atuação abrange aspectos curativos, preventivos e de pesquisa."
     },
     {
         titulo: "Urologia",
-        descricao: "Cuida de pênis"
+        descricao: "Especialidade cirúrgica da medicina que trata do trato urinário de homens e de mulheres e do sistema reprodutor das pessoas do sexo masculino."
     },
     {
         titulo: "Neurologia",
-        descricao: "Cuida de neurônio"
+        descricao: "Especialidade médica que se dedica ao estudo e tratamento das doenças que atingem o sistema nervoso central e periférico."
     },
     {
-        titulo: "Geral",
-        descricao: "Cuida do geral"
+        titulo: "Ortopedia",
+        descricao: "Especialidade médica que cuida da saúde relacionadas aos elementos do aparelho locomotor, como ossos, músculos, ligamentos e articulações."
     }
 ];
 
@@ -24,7 +25,7 @@ const Especialidades = () => especialidades.map(especialidade => (
     <li className="list-group-item" style={{marginTop: "20px"}}>
         <span><b>{especialidade.titulo}</b></span>
         <br/>
-        <span style={{marginTop: "5px"}}>{especialidade.descricao}</span>
+        <span style={{marginTop: "15px"}}>{especialidade.descricao}</span>
     </li>
 ));
 
