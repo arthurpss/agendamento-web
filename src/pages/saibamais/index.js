a6757a14e43b9e37e943b0b4e8c5fe39583ca252
Arthur Passos <arthur.pcorrea@semprecub.com>
2019-12-01 22:06:21 -0300

    Pequenas correções na tela de serviços

--- src/pages/saibamais/index.js
+++ src/pages/saibamais/index.js
@@ -41,46 +41,34 @@ export default class Saibamais extends Component {
                 hora_marcada: "Especialidade médica que investiga, diagnostica, trata e acompanha enfermidades relacionadas com fraturas e lesões ósseas e tendinosas provocadas por eventos traumáticos no aparelho músculo-esquelético ou locomotor, composto por: braços, mãos, pés, pernas, coluna , bacia, músculos, tendões e ligamentos."
             },
             {
-              data: "Fisioterapia",
-              nome: "Dr. Kronabuer",
-              medico: medico,
-              primeira_consulta: true,
-              motivo: "motivo",
-              plano: "Amil",
-              hora_marcada: "É uma ciência da saúde aplicada ao estudo, diagnóstico, prevenção e tratamento de disfunções cinéticas funcionais de órgãos e sistemas. Ela estuda, diagnostica, previne e trata os distúrbios, entre outros, cinético-funcionais (da biomecânica e funcionalidade humana) decorrentes de alterações de órgãos e sistemas humanos."
-          },
-          {
-            data: "Fisiatria",
-            nome: "Dr. Santana",
-            medico: medico,
-            primeira_consulta: true,
-            motivo: "motivo",
-            plano: "Amil",
-            hora_marcada: "Tem como foco a funcionalidade do organismo. Esta especialidade médica dedica-se à prevenção, diagnóstico e tratamento não cirúrgico de distúrbios associados à deficiência física em geral. Portanto, esta especialidade atende os pacientes que necessitam de tratamento e reabilitação física."
-        },
-        {
-          data: "Psicodiagnóstico",
-          nome: "Dr. Passos",
-          medico: medico,
-          primeira_consulta: true,
-          motivo: "motivo",
-          plano: "Amil",
-          hora_marcada: "É o processo de avaliação onde o principal objetivo é identificar e compreender a queixa e os possíveis distúrbios que o cliente apresenta para que o terapeuta consiga formular hipóteses diagnósticas mais precisas relacionadas às questões levantadas."
-      }
+                data: "Fisioterapia",
+                nome: "Dr. Kronabuer",
+                medico: medico,
+                primeira_consulta: true,
+                motivo: "motivo",
+                plano: "Amil",
+                hora_marcada: "É uma ciência da saúde aplicada ao estudo, diagnóstico, prevenção e tratamento de disfunções cinéticas funcionais de órgãos e sistemas. Ela estuda, diagnostica, previne e trata os distúrbios, entre outros, cinético-funcionais (da biomecânica e funcionalidade humana) decorrentes de alterações de órgãos e sistemas humanos."
+            },
+            {
+                data: "Fisiatria",
+                nome: "Dr. Santana",
+                medico: medico,
+                primeira_consulta: true,
+                motivo: "motivo",
+                plano: "Amil",
+                hora_marcada: "Tem como foco a funcionalidade do organismo. Esta especialidade médica dedica-se à prevenção, diagnóstico e tratamento não cirúrgico de distúrbios associados à deficiência física em geral. Portanto, esta especialidade atende os pacientes que necessitam de tratamento e reabilitação física."
+            },
+            {
+                data: "Psicodiagnóstico",
+                nome: "Dr. Passos",
+                medico: medico,
+                primeira_consulta: true,
+                motivo: "motivo",
+                plano: "Amil",
+                hora_marcada: "É o processo de avaliação onde o principal objetivo é identificar e compreender a queixa e os possíveis distúrbios que o cliente apresenta para que o terapeuta consiga formular hipóteses diagnósticas mais precisas relacionadas às questões levantadas."
+            }
         ];
-
-        this.confirmar = this.confirmar.bind(this);
-        this.state = {
-            confirmacao: false,
-            agendamentoACancelar: undefined
-        }
     }
-
-    confirmar(agendamento) {
-
-    }
-
-
 
     render() {
         return (
@@ -91,7 +79,7 @@ export default class Saibamais extends Component {
                         <th scope="col">Serviços:</th>
                         <th scope="col">Descrição</th>
                         <th scope="col">Médicos</th>
-                        <th scope="col">Valor</th>
+                        <th scope="col">Especialidade</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -106,8 +94,6 @@ export default class Saibamais extends Component {
                                     <td>
                                         <button type="button"
                                                 className="btn btn-success"
-                                                data-toggle="modal"
-                                                data-target="#modalConfirmacao"
                                         >Quero Agendar
                                         </button>
                                     </td>
@@ -117,25 +103,6 @@ export default class Saibamais extends Component {
                     }
                     </tbody>
                 </table>
-                <div className="modal fade" id="modalConfirmacao" tabIndex="-1" role="dialog"
-                     aria-labelledby="modalConfirmacao" aria-hidden="true">
-                    <div className="modal-dialog modal-dialog-centered" role="document">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title" id="modalConfirmacaoTitle">Consulta confirmada!</h5>
-                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                                Lembre-se de trazer os documentos e exames necessários. :)
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-primary" data-dismiss="modal">Fechar</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
         );
     }
